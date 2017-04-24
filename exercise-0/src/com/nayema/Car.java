f2ba0d280a602027b74dd1535c39f057247dc7e1
Nayema Nasir <nayema.nasir89@gmail.com>
2017-04-23 22:19:25 -0400

    Updates exercise-0

--- exercise-0/src/com/nayema/Car.java
+++ exercise-0/src/com/nayema/Car.java
@@ -1,11 +1,11 @@
 package com.nayema;
 
 public class Car {
-    String make;
+    String make; //These are attributes
     String model;
     int year;
 
-    public String description() {
+    public String description() { //This is a method
         return make + " " + model + " " + year;
     }
 }
