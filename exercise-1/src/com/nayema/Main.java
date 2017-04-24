b535dedccba03805d1c6c15c11cd665569b85a7b
Nayema Nasir <nayema.nasir89@gmail.com>
2017-04-24 00:47:21 -0400

    Adds name counter

--- exercise-1/src/com/nayema/Main.java
+++ exercise-1/src/com/nayema/Main.java
@@ -1,8 +1,16 @@
 package com.nayema;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("What is your name?");
+
+        Scanner nameScanner = new Scanner(System.in); //Creates a new scanner
+        String name = nameScanner.nextLine(); //Asks to put your name
+        int length = name.length(); //counts the length of the String
+
+        System.out.println("Hello " + name + " you have " + length + " letters in your name.");
     }
 }
