a7ae0095a22b9ac8f34494fe4a2cf3411162724b
Nayema Nasir <nayema.nasir89@gmail.com>
2017-04-23 21:58:14 -0400

    Moves to subfolders

---
+++ exercise-0/src/com/nayema/Car.java
@@ -0,0 +1,11 @@
+package com.nayema;
+
+public class Car {
+    String make;
+    String model;
+    int year;
+
+    public String description() {
+        return make + " " + model + " " + year;
+    }
+}
