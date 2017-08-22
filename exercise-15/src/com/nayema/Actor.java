ce02aa0c2b8b04b577076ce216fd454b30e2bf94
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-22 18:57:45 -0400

    Adds factory method

--- exercise-15/src/com/nayema/Actor.java
+++ exercise-15/src/com/nayema/Actor.java
@@ -1,15 +1,39 @@
 package com.nayema;
 
 public class Actor {
-    private static String[] name = {"Arnold Schwarzenegger", "Will Smith", "Anglenia Jolie", "Brad Pitt"};
+    private static String[] possibleNames = {
+            "Arnold Schwarnezeggar",
+            "Angelina Jolie",
+            "Brad Pitt",
+            "Will Smith",
+            "Leonardo Decaprio",
+            "Johny Depp",
+            "Lisa Ray",
+            "Jamie Foxx",
+            "Tom Hanks",
+            "Matt Damon",
+            "Jennifer Lawrence",
+            "Robin Williams",
+            "Ben Affleck",
+            "Bruce Willis",
+            "Vin Diesel",
+            "Julia Roberts",
+            "Megan Fox"
+    };
 
-    private Actor() {
+    public static Actor createRandomActor() {
+        return new Actor(getActorName());
     }
 
-    public static void randomActorName() {
-        int length = name.length;
-        int random = (int)(Math.random() * length);
-        String actorName = name[random];
-        System.out.println(" I am " + actorName + "!");
+    private static String getActorName() {
+        int length = possibleNames.length;
+        int random = (int) (Math.random() * length);
+        return possibleNames[random];
+    }
+
+    String name;
+
+    private Actor(String name) {
+        this.name = name;
     }
 }
--- exercise-15/src/com/nayema/Main.java
+++ exercise-15/src/com/nayema/Main.java
@@ -3,6 +3,9 @@ package com.nayema;
 public class Main {
 
     public static void main(String[] args) {
-        Actor.randomActorName();
+
+        Actor actor = Actor.createRandomActor();
+
+        System.out.println("I am " + actor.name + "!");
     }
 }
