a5f0b2eb1fe0031b930fc2c9737d40bad4b9a2e0
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-06 18:59:31 -0400

    Moves methods from Coordinates class to Main

--- exercise-18/src/com/nayema/Coordinates.java
+++ exercise-18/src/com/nayema/Coordinates.java
@@ -4,24 +4,8 @@ public class Coordinates {
     public int rowNumber;
     public int columnNumber;
 
-    public Coordinates findCoordinates(int[][] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers[i].length; j++) {
-                if (numbers[i][j] == 5) {
-                    rowNumber = i;
-                    columnNumber = j;
-                }
-            }
-        }
-        return new Coordinates();
-    }
-
-    public void printCoordinates(int[][] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers[i].length; j++) {
-                System.out.print(numbers[i][j]);
-            }
-            System.out.println();
-        }
+    public Coordinates(int rowNumber, int columnNumber) {
+        this.rowNumber = rowNumber;
+        this.columnNumber = columnNumber;
     }
 }
--- exercise-18/src/com/nayema/Main.java
+++ exercise-18/src/com/nayema/Main.java
@@ -5,12 +5,34 @@ public class Main {
     public static void main(String[] args) {
         int[][] array = {{1, 2, 3, 4, 6}, {3, 3, 1, 2, 1}, {1, 2, 3, 4, 5}};
 
-        Coordinates coordinates = new Coordinates();
-
         System.out.println("2D Array: ");
-        coordinates.printCoordinates(array);
+        printArray(array);
 
-        coordinates.findCoordinates(array);
+        Coordinates coordinates = findCoordinates(array, 5);
         System.out.println("rowNumber: " + coordinates.rowNumber + " columnNumber: " + coordinates.columnNumber);
+    }
+
+    public static Coordinates findCoordinates(int[][] numbers, int searchFor) {
+        int rowNumber = -1;
+        int columnNumber = -1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                if (numbers[i][j] == searchFor) {
+                    rowNumber = i;
+                    columnNumber = j;
+                }
+            }
+        }
+        return new Coordinates(rowNumber, columnNumber);
+    }
+
+    public static void printArray(int[][] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.print(numbers[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
