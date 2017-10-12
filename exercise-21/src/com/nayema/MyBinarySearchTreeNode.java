f215926b1307e444b9ab3f27c26eecbc240f7aec
Nayema Nasir <nayema.nasir89@gmail.com>
2017-10-12 15:15:01 -0400

    Adds instance variables and constructur to MyLinkedListNode

--- exercise-21/src/com/nayema/MyBinarySearchTreeNode.java
+++ exercise-21/src/com/nayema/MyBinarySearchTreeNode.java
@@ -1,4 +1,12 @@
 package com.nayema;
 
-public class MyBinarySearchTreeNode {
+public class MyBinarySearchTreeNode<E> {
+    public E value;
+    public int key;
+    public MyBinarySearchTreeNode<E> right;
+    public MyBinarySearchTreeNode<E> left;
+
+    public MyBinarySearchTreeNode(E value) {
+        this.value = value;
+    }
 }
