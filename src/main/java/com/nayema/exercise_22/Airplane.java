0d3c4149b0c471e296d055cae28978a247277271
Nayema Nasir <nayema.nasir89@gmail.com>
2017-10-17 22:13:35 -0400

    Initial commit of exercise_22

---
+++ src/main/java/com/nayema/exercise_22/Airplane.java
@@ -0,0 +1,8 @@
+package com.nayema.exercise_22;
+
+public class Airplane {
+    private String modelName;
+    private int seatCapacity;
+    private String nextInspectionDate;
+    private int weight;
+}
