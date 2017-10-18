cad590b150164ea8ac27a97c69c3a4d70b18fa2d
Nayema Nasir <nayema.nasir89@gmail.com>
2017-10-18 18:23:08 -0400

    Creates table model methods

--- src/main/java/com/nayema/exercise_22/AirplaneIS.java
+++ src/main/java/com/nayema/exercise_22/AirplaneIS.java
@@ -1,8 +1,15 @@
 package com.nayema.exercise_22;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
 public class AirplaneIS {
+    private JFrame frame;
     private JPanel mainPanel;
     private JTable table1;
     private JPanel editorPanel;
@@ -16,4 +23,50 @@ public class AirplaneIS {
     private JLabel inspectionDateLabel;
     private JLabel weightLabel;
     private JButton queryButton;
+    private SqlHelper sqlHelper;
+
+    public AirplaneIS() throws SQLException {
+        sqlHelper = new SqlHelper("jdbc:sqlite:/Users/Nayema/Development/java-exercises/nayema.sqlite.airplane");
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        new AirplaneIS().buildAirplaneIS();
+    }
+
+    private void buildAirplaneIS() throws SQLException {
+        populateTable();
+        renderUI();
+    }
+
+    private void populateTable() throws SQLException {
+        ResultSetMetaData resultSetMetaData = sqlHelper.queryResultSetMetaData();
+        int columnCount = resultSetMetaData.getColumnCount();
+
+        DefaultTableModel tableModel = (DefaultTableModel) table1.getModel();
+        tableModel.setColumnCount(0);
+        for (int i = 1; i < columnCount; i++) {
+            tableModel.addColumn(resultSetMetaData.getColumnName(i));
+        }
+        tableModel.setRowCount(0);
+        ResultSet resultSet = sqlHelper.getResultSet();
+        while (resultSet.next()) {
+            String[] record = new String[columnCount];
+            for (int i = 0; i < columnCount; i++) {
+                record[i] = resultSet.getString(i + 1);
+            }
+            tableModel.addRow(record);
+        }
+        tableModel.fireTableDataChanged();
+        resultSet.close();
+    }
+
+    private void renderUI() {
+        frame = new JFrame("Airplane Information System");
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 800));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
--- src/main/java/com/nayema/exercise_22/SqlHelper.java
+++ src/main/java/com/nayema/exercise_22/SqlHelper.java
@@ -4,21 +4,34 @@ import java.sql.*;
 
 public class SqlHelper {
     private Connection connection;
+    private ResultSet resultSet;
+
+    public ResultSet getResultSet() {
+        return resultSet;
+    }
 
     public SqlHelper(String url) throws SQLException {
         connection = DriverManager.getConnection(url);
     }
 
-    public void clearTable() throws SQLException {
-        String query = "DELETE FROM airplane_data";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-        preparedStatement.executeUpdate();
-        preparedStatement.close();
+    public void insertRecord(String modelName, int seatCapacity, String nextInspectionDate, int weight) throws SQLException {
+        String sql = "INSERT INTO airplane_data(model_name, seat_capacity, next_inspection_date, weight) VALUES (?, ?, ?, ?)";
+        insertNewAirplaneRecord(modelName, seatCapacity, nextInspectionDate, weight, sql);
     }
 
-    public void insertNewAirplaneRecord(String modelName, int seatCapacity, String nextInspectionDate, int weight) throws SQLException {
-        String query = "INSERT INTO airplane_data(model_name, seat_capacity, next_inspection_date, weight) VALUES (?, ?, ?, ?)";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
+    public void queryData(String modelNameQuery) throws SQLException {
+        String sql = "SELECT * FROM airplane_data WHERE model_name = ?";
+        queryByModelName(modelNameQuery, sql);
+    }
+
+    public ResultSetMetaData queryResultSetMetaData() throws SQLException {
+        String sql = "SELECT * FROM airplane_data";
+        return getMetaData(sql);
+
+    }
+
+    private void insertNewAirplaneRecord(String modelName, int seatCapacity, String nextInspectionDate, int weight, String sql) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, modelName);
         preparedStatement.setInt(2, seatCapacity);
         preparedStatement.setString(3, nextInspectionDate);
@@ -27,9 +40,14 @@ public class SqlHelper {
         preparedStatement.close();
     }
 
-    public void queryData(String modelNameQuery) throws SQLException {
-        String query = "SELECT * FROM airplane_data WHERE model_name = ?" ;
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
+    private ResultSetMetaData getMetaData(String sql) throws SQLException {
+        Statement statement = connection.createStatement();
+        resultSet = statement.executeQuery(sql);
+        return resultSet.getMetaData();
+    }
+
+    private void queryByModelName(String modelNameQuery, String sql) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, modelNameQuery);
         preparedStatement.executeQuery();
     }
