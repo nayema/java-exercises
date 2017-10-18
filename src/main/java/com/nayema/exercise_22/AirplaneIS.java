fd8021fe9cbab131800e4bf6064637370875997e
Nayema Nasir <nayema.nasir89@gmail.com>
2017-10-18 17:14:06 -0400

    Creates a GUI for Airplane Information System

---
+++ src/main/java/com/nayema/exercise_22/AirplaneIS.java
@@ -0,0 +1,19 @@
+package com.nayema.exercise_22;
+
+import javax.swing.*;
+
+public class AirplaneIS {
+    private JPanel mainPanel;
+    private JTable table1;
+    private JPanel editorPanel;
+    private JButton insertButton;
+    private JTextField modelNameTextField;
+    private JTextField seatCapacityTextField;
+    private JTextField nextInspectionDateTextField;
+    private JTextField weightLbsTextField;
+    private JLabel modelNameLabel;
+    private JLabel seatCapacityLabel;
+    private JLabel inspectionDateLabel;
+    private JLabel weightLabel;
+    private JButton queryButton;
+}
