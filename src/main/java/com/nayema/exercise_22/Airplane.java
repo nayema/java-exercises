d39b21ee7aeaa0e23cb8c275c685bf5d5f2a4df3
Nayema Nasir <nayema.nasir89@gmail.com>
2017-10-19 17:44:33 -0400

    Changes contructor to accept all String parameters

--- src/main/java/com/nayema/exercise_22/Airplane.java
+++ src/main/java/com/nayema/exercise_22/Airplane.java
@@ -6,10 +6,10 @@ public class Airplane {
     public String nextInspectionDate;
     public int weight;
 
-    public Airplane(String modelName, int seatCapacity, String nextInspectionDate, int weight) {
+    public Airplane(String modelName, String seatCapacity, String nextInspectionDate, String weight) {
         this.modelName = modelName;
-        this.seatCapacity = seatCapacity;
+        this.seatCapacity = Integer.parseInt(seatCapacity);
         this.nextInspectionDate = nextInspectionDate;
-        this.weight = weight;
+        this.weight = Integer.parseInt(weight);
     }
 }
